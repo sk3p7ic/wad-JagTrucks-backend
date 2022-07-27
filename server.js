bf9bf80a9169bd92c2eafb2584002ec6977d9df8
Joshua Ibrom <joshuaibrom@gmail.com>
2022-07-27 00:35:15 -0500

    Boilerplate code for express app

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const port = 5000;
+
+const app = express();
+
+app.get('/', (req, res) => {res.send('Hello world!');});
+
+app.listen(port, () => {console.log(`Listening on port ${port}`)});
